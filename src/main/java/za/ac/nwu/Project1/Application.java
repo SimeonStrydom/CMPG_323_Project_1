7404b0eadc100af14b30635dea0b330e26f53366
SS <simeon.strydom@gmail.com>
2021-09-15 11:47:20 +0200

    Initial commit

---
+++ src/main/java/za/ac/nwu/Project1/Application.java
@@ -0,0 +1,23 @@
+package za.ac.nwu.Project1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Autowired
+	@GetMapping("/Hello")
+	public String test(){
+		return "Testing testing 1 2 3";
+	}
+
+}
