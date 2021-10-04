12cc9c296501ca53b6c943170ee506e692e1e442
SS <simeon.strydom@gmail.com>
2021-10-04 17:32:13 +0200

    DB fix, working build
    Added nativeQuery = true to all queries. Build is successful.

--- src/main/java/za/ac/nwu/Project1/Database/queries/AddressRepository.java
+++ src/main/java/za/ac/nwu/Project1/Database/queries/AddressRepository.java
@@ -12,32 +12,32 @@ import javax.transaction.Transactional;
 public interface AddressRepository extends CrudRepository<Address, Long> {
     @Modifying
     @Transactional
-    @Query(value = "INSERT INTO Address(CityTown, Suburb, StreetName, StreetNumber) VALUES(?1, ?2, ?3, ?4)")
+    @Query(value = "INSERT INTO Address(CityTown, Suburb, StreetName, StreetNumber) VALUES(?1, ?2, ?3, ?4)", nativeQuery = true)
     void addAddress(String cityTown, String suburb, String streetName, Long streetNumber);
 
     @Transactional
-    @Query(value = "SELECT * FROM Address WHERE AddressID = ?1")
+    @Query(value = "SELECT * FROM Address WHERE AddressID = ?1", nativeQuery = true)
     void findAddressByID(Long addressID);
     // Is more SELECT's necessary?
 
     @Modifying
     @Transactional
-    @Query(value = "UPDATE Address SET CityTown = ?2 WHERE AddressID = ?1")
+    @Query(value = "UPDATE Address SET CityTown = ?2 WHERE AddressID = ?1", nativeQuery = true)
     void updateCityTown(Long addressID, String newCityName);
 
     @Modifying
     @Transactional
-    @Query(value = "UPDATE Address SET Suburb = ?2 WHERE AddressID = ?1")
+    @Query(value = "UPDATE Address SET Suburb = ?2 WHERE AddressID = ?1", nativeQuery = true)
     void updateSuburb(Long addressID, String newSuburb);
 
     @Modifying
     @Transactional
-    @Query(value = "UPDATE Address SET StreetName = ?2 WHERE AddressID = ?1")
+    @Query(value = "UPDATE Address SET StreetName = ?2 WHERE AddressID = ?1", nativeQuery = true)
     void updateStreetName(Long addressID, String newStreetName);
 
     @Modifying
     @Transactional
-    @Query(value = "UPDATE Address SET StreetNumber = ?2 WHERE AddressID = ?1")
+    @Query(value = "UPDATE Address SET StreetNumber = ?2 WHERE AddressID = ?1", nativeQuery = true)
     void updateStreetNumber(Long addressID, String newStreetNumber);
 
 }
--- src/main/java/za/ac/nwu/Project1/Database/queries/MemberRepository.java
+++ src/main/java/za/ac/nwu/Project1/Database/queries/MemberRepository.java
@@ -14,53 +14,53 @@ import javax.transaction.Transactional;
 public interface MemberRepository extends CrudRepository<Member, Long> {
     @Modifying
     @Transactional
-    @Query(value = "INSERT INTO Member(FirstName, LastName, ContactNumber, Email, AccountID, AddressID, IsActive) VALUES(?, ?, ?, ?, ?, ?, ?)")
+    @Query(value = "INSERT INTO Member(FirstName, LastName, ContactNumber, Email, AccountID, AddressID, IsActive) VALUES(?, ?, ?, ?, ?, ?, ?)", nativeQuery = true)
     void addMember(String firstName, String lastName, Long contactNumber, String email, Long FKAccountID, Long FKAddressID, Boolean isActive);
 
 
     @Transactional
-    @Query(value = "SELECT * FROM Member WHERE MemberID = ?1")
+    @Query(value = "SELECT * FROM Member WHERE MemberID = ?1", nativeQuery = true)
     void findMemberByID(Long id);
 
     @Transactional
-    @Query(value = "SELECT * FROM Member WHERE IsActive = true")
+    @Query(value = "SELECT * FROM Member WHERE IsActive = true", nativeQuery = true)
     void isActiveMember();
 
     @Transactional
-    @Query(value = "SELECT * FROM Member WHERE IsActive = false")
+    @Query(value = "SELECT * FROM Member WHERE IsActive = false", nativeQuery = true)
     void isInactiveMember();
 
     // add more select statements?
 
     @Modifying
     @Transactional
-    @Query(value = "UPDATE Member SET FirstName = ?2 WHERE MemberID = ?1 ")
+    @Query(value = "UPDATE Member SET FirstName = ?2 WHERE MemberID = ?1 ", nativeQuery = true)
     void updateMemberFirstName(Long memberID, String newFirstName);
 
     @Modifying
     @Transactional
-    @Query(value = "UPDATE Member SET LastName = ?2 WHERE MemberID = ?1 ")
+    @Query(value = "UPDATE Member SET LastName = ?2 WHERE MemberID = ?1 ", nativeQuery = true)
     void updateMemberLastName(Long memberID, String newLastName);
 
     @Modifying
     @Transactional
-    @Query(value = "UPDATE Member SET ContactNumber = ?2 WHERE MemberID = ?1 ")
+    @Query(value = "UPDATE Member SET ContactNumber = ?2 WHERE MemberID = ?1 ", nativeQuery = true)
     void updateMemberContactNumber(Long memberID, Long newContactNumber);
 
     @Modifying
     @Transactional
-    @Query(value = "UPDATE Member SET Email = ?2 WHERE MemberID = ?1 ")
+    @Query(value = "UPDATE Member SET Email = ?2 WHERE MemberID = ?1 ", nativeQuery = true)
     void updateMemberEmail(Long memberID, String newEmail);
 
     @Modifying
     @Transactional
-    @Query(value = "UPDATE Member SET IsActive = ?2 WHERE MemberID = ?1 ")
+    @Query(value = "UPDATE Member SET IsActive = ?2 WHERE MemberID = ?1 ", nativeQuery = true)
     void updateMemberIsActive(Long memberID, Boolean isActive);
 
     //Must there be a delete?
     @Modifying
     @Transactional
-    @Query(value = "DELETE FROM Member WHERE MemberID = ?1")
+    @Query(value = "DELETE FROM Member WHERE MemberID = ?1", nativeQuery = true)
     void deleteMember(Long memberID);
 
 }
