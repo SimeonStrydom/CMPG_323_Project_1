6d75a8e06a8042d394df83f627ada009fe53bf7e
SS <simeon.strydom@gmail.com>
2021-10-04 20:55:54 +0200

    Added unit testing dependencies

---
+++ src/test/java/za/ac/nwu/Project1/Database/entities/AccountTest.java
@@ -0,0 +1,38 @@
+package za.ac.nwu.Project1.Database.entities;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAccountID() {
+    }
+
+    @Test
+    void setAccountID() {
+    }
+
+    @Test
+    void getMiles() {
+    }
+
+    @Test
+    void setMiles() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
